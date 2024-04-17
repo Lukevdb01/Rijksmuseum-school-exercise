b6f5e356e95cd3e14db2ea2a1f61d2cec433c9bc
SamedYasin <Samedbozaslan25@gmail.com>
2024-04-17 13:26:21 +0200

    .

--- src/pages/information.vue
+++ src/pages/information.vue
@@ -1,17 +1,35 @@
 <script setup>
-
+import TabBar from "../components/TabBar.vue";
+import {ref} from "vue";
+//Painting
+const paintingImg = ref();
+const paintingTitle = ref();
+const paintingDate = ref();
+const paintingInfo = ref();
+//painter
+const painterImg = ref();
+const painterName = ref();
+const painterDate = ref();
 </script>
 
+
 <template>
+
     <div class="base base-container">
         <h3>Information</h3>
         <div class="container">
             <p>Information about the app</p>
-            <TabBar />
         </div>
     </div>
 </template>
 
-<style scoped>
+<script>
 
+</script>
+
+<style scoped>
+.base-container{
+  color: var(--primary-text-color);
+  background-color: var(--primary-background-color);
+}
 </style>

f2bbc9b7e2d29c7aa545177a0241ce05abf1ba55
Luke <luckyskywalker2003@gmail.com>
2024-04-17 13:25:35 +0200

    I got the QR scanner working!

--- src/pages/qr-scanner.vue
+++ src/pages/qr-scanner.vue
@@ -3,50 +3,60 @@
     import { QrcodeStream } from 'vue-qrcode-reader';
 </script>
 
-<script>
-export default {
-    data() {
-        return {
-            error: '',
-        }
-    },
-
-    components: { QrcodeStream },
-    methods: {
-        onInit(promise) {
-            try {
-                const {capabilities} = await promise;
-            } catch (error) {
-                if (error.name === 'NotAllowedError') {
-                this.error = 'user denied camera access permission';
-                } else if (error.name === 'NotFoundError') {
-                // no suitable camera device installed
-                } else if (error.name === 'NotSupportedError') {
-                // page is not served over HTTPS (or localhost)
-                } else if (error.name === 'NotReadableError') {
-                // maybe camera is already in use
-                } else if (error.name === 'OverconstrainedError') {
-                // did you request the front camera although there is none?
-                } else if (error.name === 'StreamApiNotSupportedError') {
-                // browser seems to be lacking features
-                }
-            }
-        }
-    }
-}
-</script>
-
 <template>
     <div class="base base-container">
         <h3>scan the qr code</h3>
         <div class="container">
-            <div class="flex-image-qr image scan-container">
-                <qrcode-stream @init="onInit"></qrcode-stream>
+            <p v-if="error" class="error">{{ error }}</p>
+            <div class="base flex-image-qr">
+                <qrcode-stream @error="onError" @detect="onDetect" @decode="onDecode" class="qr-stream"></qrcode-stream>
             </div>
             <TabBar />
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            error: '',
+            content: '',
+            result: '',
+        }
+    },
+
+    components: { QrcodeStream },
+    methods: {
+        async onError(promise) {
+            try {
+                const {capabilities} = await promise
+            } catch (error) {
+                if (error.name === 'NotAllowedError') {
+                    this.error = 'user denied camera access permission';
+                } else if (error.name === 'NotFoundError') {
+                    this.error = 'no suitable camera device installed';
+                } else if (error.name === 'NotSupportedError') {
+                    this.error = 'page is not served over HTTPS (or localhost)';
+                } else if (error.name === 'NotReadableError') {
+                    this.error = 'maybe camera is already in use';
+                } else if (error.name === 'OverconstrainedError') {
+                    this.error = 'did you request the front camera although there is none?';
+                } else if (error.name === 'StreamApiNotSupportedError') {
+                    this.error = 'browser seems to be lacking features';
+                }
+            }
+        },
+        onDetect(codes) {
+            this.result = JSON.stringify(codes.map((code) => code.rawValue));
+        },
+        onDecode(content) {
+            console.log(content);
+            this.content = content;
+        },
+    }
+}
+</script>
 
 <style scoped>
     .base-container {
@@ -66,6 +76,7 @@ export default {
         align-items: center;
         padding: calc(1rem + 1vw);
         gap: calc(1rem + 1vw);
+        color: black;
     }
 
     .flex-image-qr {
@@ -74,6 +85,12 @@ export default {
         align-items: center;
         background-color: var(--primary-background-color);
         border-radius: calc(1.5rem + 0.5vw);
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    .qr-stream {
         width: 100%;
         height: 100%;
     }

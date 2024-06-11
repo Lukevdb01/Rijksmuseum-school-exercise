033d52d4039d62bf60ccecf90bcb7a9fcf3a127d
Luke <luckyskywalker2003@gmail.com>
2024-06-11 11:43:09 +0200

    Added firebase API keys in .env file

--- src/main.js
+++ src/main.js
@@ -1,31 +1,35 @@
+// Vue imports
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 import router from './services/routing';
 import { createHead } from '@vueuse/head';
+
+// Firebase imports
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import app from "./App.vue";
+import 'firebase/auth';
+
+// Misc imports
+import './style.css';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCcWPrLrL13TLTo8ieMyAPBYoxZchu4B-4",
-    authDomain: "samed-luuk-rijksapi.firebaseapp.com",
-    projectId: "samed-luuk-rijksapi",
-    storageBucket: "samed-luuk-rijksapi.appspot.com",
-    messagingSenderId: "531933662431",
-    appId: "1:531933662431:web:758dea8ab5b42a4988ad6a",
-    measurementId: "G-TSB9B1ETWB"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
+initializeApp(firebaseConfig);
 
 // Initialize Vue application
 const vueApp = createApp(App);
 const head = createHead();
 
+// Mount Vue application
 vueApp.use(router);
 vueApp.use(head);
 
